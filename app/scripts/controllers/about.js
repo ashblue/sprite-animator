160c8e4bf682a5550560706e0f28f3385697e8f6
ashblue <ash@blueashes.com>
2014-06-15 18:45:06 -0700

    Stabilized Yeoman install

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spriteAnimatorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the spriteAnimatorApp
+ */
+angular.module('spriteAnimatorApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spriteAnimatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spriteAnimatorApp
+ */
+angular.module('spriteAnimatorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
